3429f7cc66ba63b2f71a092a2272170c255cd712
Kristel <kristel.mercurino@hotmail.com>
2024-12-16 19:30:38 -0300

    weather api sms working

---
+++ twilio_config_template.py
@@ -0,0 +1,8 @@
+# Archivo: twilio_config_template.py
+# Plantilla de configuración para credenciales (vacío para repositorio).
+
+API_KEY_WAPI = ""  # Coloca tu API Key de WeatherAPI
+TWILIO_ACCOUNT_SID = ""  # Coloca tu Twilio Account SID
+TWILIO_AUTH_TOKEN = ""  # Coloca tu Twilio Auth Token
+PHONE_NUMBER = ""  # Número de Twilio
+TO_PHONE_NUMBER = ""  # Número de destino (tu teléfono personal)
